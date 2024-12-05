83508797381d5b1be6c7daffd1583a4d058543fb
weishao-yu <yweiwei1005@gmail.com>
2024-12-05 14:37:07 +0800

    text:修正錯誤

--- prime_numbers.c
+++ prime_numbers.c
@@ -3,11 +3,14 @@ int main()
 {
     int n;
     scanf("%d", &n);
+    if (n == 2)
+        printf("%d is a prime number\n", n);
     for (int i = 2; i < n; i++)
     {
         if (n % i == 0)
         {
             printf("%d is not a prime number\n", n);
+            break;
         }
         if (i == n - 1)
         {
